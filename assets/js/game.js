e32a7244fe5c43ed885ac2491806485d5893f3d0
jorddo <jordanbsneed@gmail.com>
2021-12-31 08:22:50 -0600

    add enemy conbatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,15 +3,15 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// can also log multiple values at once, like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = 'Roborto';
+var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+// GAME STATES
+// "WIN" - player robot has defeated all enemy-robots
+//    *fight all enemy-robots
 // creates function
-var fight = function () {
+var fight = function (enemyName) {
   // Alert players that they are starting the round
   window.alert('Welcome to Robot Gladiators!');
 
@@ -27,9 +27,9 @@ var fight = function () {
     console.log(
       playerName +
         ' attacked ' +
-        enemyName +
+        enemyNames +
         '. ' +
-        enemyName +
+        enemyNames +
         ' now has ' +
         enemyHealth +
         ' health remaining.'
@@ -37,15 +37,15 @@ var fight = function () {
 
     // check enemy's health
     if (enemyHealth <= 0) {
-      window.alert(enemyName + ' has died!');
+      window.alert(enemyNames + ' has died!');
     } else {
-      window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
+      window.alert(enemyNames + ' still has ' + enemyHealth + ' health left.');
     }
 
     // remove player's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
-      enemyName +
+      enemyNames +
         ' attacked ' +
         playerName +
         '. ' +
@@ -78,6 +78,12 @@ var fight = function () {
     }
   }
 };
+//    *defeat each enemy-robot
+for (var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
+
+// "LOSE" - player robot's health is zero or less
 
 // executes function
-fight();
+//fight();
