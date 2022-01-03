d30b6e3684df465f9ae5e0ccd00fe11af35ba79b
jorddo <jordanbsneed@gmail.com>
2022-01-03 12:21:18 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,14 +24,16 @@ var fight = function (enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log('playerMoney', playerMoney);
         break;
       }
     }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    // generate random damage value based on player's attack power
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName +
         ' attacked ' +
@@ -56,8 +58,9 @@ var fight = function (enemyName) {
       window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
     }
 
-    // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName +
         ' attacked ' +
@@ -95,7 +98,7 @@ var startGame = function () {
       // pick new enemy to fight
       var pickedEnemyName = enemyNames[i];
       // reset enemy health before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
       //debugger
       //pass the pickedEnemyName variables into the fight function
       fight(pickedEnemyName);
@@ -188,6 +191,13 @@ var shop = function () {
       shop();
       break;
   }
+};
+
+// function to generate random numeric value
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
 };
 
 // start game when page loads
